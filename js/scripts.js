cc8fa761e1dd3798335c72f4963ffc2cb5547685
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-17 00:43:45 +0300

    add slideshow on html,css and js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function slideSwitch() {
+
+    $('img').css('display','none');
+    var $active = $('#slideshow1 IMG.active');
+    
+    if ( $active.length == 0 ){ 
+        $active = $('#slideshow1 IMG:last');
+    }
+    
+    var $next;
+    if ($active.next().length){
+        $next = $active.next();
+    }else{
+        $next = $('#slideshow1 IMG:first');
+    }
+    
+    $active.addClass('last-active');
+    
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .css('display','block')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+    }
+    
+    $(function() {
+      setInterval( "slideSwitch()", 3000 );
+    });
