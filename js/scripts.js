f3d1fba0581524d856db7917be2da13d701f8225
Cynthia Kitili <nyambucindy0@gmail.com>
2020-10-18 21:28:28 +0300

    add jquery constructor and prototype

--- js/scripts.js
+++ js/scripts.js
@@ -61,3 +61,73 @@ function slideSwitch() {
     $(function() {
       setInterval( "slideSwitch()", 5000 );
     });
+ 
+    
+/*var myName=name;
+var orderSize=size;
+var orderCrust=crust;
+var orderToppings=toppings;
+var toppingsArray=[];
+var toppingsNumber=toppingsNo;
+var pizzaNumber=number;
+var total=0;
+
+function order(name,size,crust,toppings,number,price){
+  this.name=name;
+  this.size=size;
+  this.crust=crust;
+  this.toppings=toppings;
+  this.number = number;
+  this.price=price ;}
+
+  $('#orderForm').submit(function(event){
+    event.preventDefault();  
+    orderSize= $('#size').val();
+    orderCrust= $("#crust").val();
+    pizzaNumber= $("#number").val();
+    var address=$("#street").val();
+    $('div#toppings:radio:selected').each(function(i){
+      orderToppings[i] = $(this).val();
+      toppingsArray[i] = $(this).attr('name')}*/
+
+function order(toppings, size,crust){
+  this.toppings = toppings,
+  this.size = size,
+  this.crust=crust,
+}
+
+order.prototype.toppingsAmount= function(){
+  if(this.toppings.length != 0){
+    var toppingPrice = this.toppings.length *3;
+    return toppingPrice;
+  }else {
+    alert("please choose your Toppings !!")
+  }
+}
+order.prototype.sizeAmount = function(){
+  if (this.size === "Small"){
+    return 4;
+  } else if(this.size === "Medium"){
+    return 5;
+  } else if(this.size === "Large"){
+    return 6;
+  } else {
+    alert ("Please select a size that you would prefer");
+  }
+}
+order.prototype.crustAmount= function(){
+  if(this.crust.length != 0){
+    var crustPrice = this.crust.length *3;
+    return crustPrice;
+  }else {
+    alert("please choose your crust !!")
+  }
+}
+order.prototype.totalCost = function(toppingAmount, sizeAmount, crustAmount){
+  var totalCost =toppingAmount +sizeAmount+crustAmount;
+  $("#output")
+}
+
+
+
+
